ca96415ea8721856fe157a4a1110b66e08eeba95
root <root@Tammu>
2024-08-31 10:58:53 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,86 +1,114 @@
 import sys
-# import pyparsing - available if you need it!
-# import lark - available if you need it!
-class Pattern:
-    DIGIT = "\d"
-    ALNUM = "\w"
-# def combined_patterns(input_line, pattern):
-def match_pattern(input_line, pattern):
-    if len(input_line) == 0 and len(pattern) == 0:
-        return True
-    if not pattern:
-        return True
-    if not input_line:
-        return False
-    if pattern[0] == "^":
-        pattern = pattern[1:]
-        while len(pattern) > 0 and len(input_line) > 0:
-            if pattern[0] == input_line[0]:
-                pattern = pattern[1:]
-                input_line = input_line[1:]
-            else:
-                return False
-        return match_pattern(input_line, pattern)
-    if pattern[-1] == "$":
-        pattern = pattern[:-1]
-        while len(pattern) > 0 and len(input_line) > 0:
-            if pattern[-1] == input_line[-1]:
-                pattern = pattern[:-1]
-                input_line = input_line[:-1]
-            else:
-                return False
-        return match_pattern(input_line, pattern)
-    if pattern[0] == input_line[0]:
-        if len(pattern) > 1:
-            if pattern[1] == "+":
-                return match_pattern(input_line[1:], pattern[2:]) or match_pattern(
-                    input_line[1:], pattern
-                )
-            elif pattern[1] == "?":
-                return match_pattern(input_line[1:], pattern[2:]) or match_pattern(
-                    input_line, pattern[1:]
-                )
-        return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[0] != input_line[0] and len(pattern) > 1 and pattern[1] == "?":
-        if pattern[1] == "?":
-            return match_pattern(input_line, pattern[2:])
-        return False
-    elif pattern[0] == ".":
-        return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[:2] == Pattern.DIGIT:
-        for i in range(len(input_line)):
-            if input_line[i].isdigit():
-                return match_pattern(input_line[i:], pattern[2:])
+def match_digit(char: str):
+    return ord("0") <= ord(char) <= ord("9")
+def match_alphabets(char: str):
+    is_upper = ord("A") <= ord(char) <= ord("Z")
+    is_lower = ord("a") <= ord(char) <= ord("z")
+    return is_upper or is_lower
+def match_alphanum(char: str):
+    return match_alphabets(char) or match_digit(char) or char == "_"
+def match_pcg(char: str, group: str):
+    return char in group
+def match_ncg(char: str, group: str):
+    return char not in group
+def is_ncgp(pattern):
+    return len(pattern) >= 4 and pattern[0:2] == "[^" and pattern[-1] == "]"
+def is_pcgp(pattern):
+    return len(pattern) >= 3 and pattern[0] == "[" and pattern[-1] == "]"
+def is_alphap(pattern):
+    return pattern == "\\w"
+def is_digitp(pattern):
+    return pattern == "\\d"
+def match_character(char: str, pattern: str):
+    if is_ncgp(pattern):
+        return match_ncg(char, pattern)
+    if is_pcgp(pattern):
+        return match_pcg(char, pattern)
+    if is_alphap(pattern):
+        return match_alphanum(char)
+    if is_digitp(pattern):
+        return match_digit(char)
+    if pattern == ".":
+        return bool(char)
+    return pattern == char
+def parse(regex: str):
+    j = 0
+    pattern = []
+    while j < len(regex):
+        if regex[j : j + 2] == "\\d":
+            j += 2
+            pattern.append("\\d")
+        elif regex[j : j + 2] == "\\w":
+            j += 2
+            pattern.append("\\w")
+        elif regex[j : j + 2] == "[^" and regex.find("]") != -1:
+            end = regex.find("]") + 1
+            pattern.append(regex[j:end])
+            j = end
+        elif regex[j : j + 1] == "[" and regex.find("]") != -1:
+            end = regex.find("]") + 1
+            pattern.append(regex[j:end])
+            j = end
         else:
-            return False
-    elif pattern[:2] == Pattern.ALNUM:
-        if input_line[0].isalnum():
-            return match_pattern(input_line[1:], pattern[2:])
-        else:
-            return False
-    elif pattern[0] == "[" and pattern[-1] == "]":
-        if pattern[1] == "^":
-            chrs = list(pattern[2:-1])
-            for c in chrs:
-                if c in input_line:
-                    return False
+            pattern.append(regex[j])
+            j += 1
+    return pattern
+def match_pattern(text: str, pattern: str):
+    regex = parse(pattern)
+    if regex and regex[0] == "^":
+        return match_here(text, regex[1:])
+    while text:
+        if match_here(text, regex):
             return True
-        chrs = list(pattern[1:-1])
-        for c in chrs:
-            if c in input_line:
+        text = text[1:]
+def match_here(text, regex):
+    if not regex:
+        return True
+    if len(regex) == 1 and regex[0] == "$":
+        return not text
+    if len(regex) == 1 and len(text) == 1:
+        return match_character(text[0], regex[0])
+    if len(regex) >= 2 and regex[1] == "+":
+        return match_plus(
+            regex[0],
+            text,
+            regex[2:],
+        )
+    if len(regex) >= 2 and regex[1] == "?":
+        return match_star(
+            regex[0],
+            text,
+            regex[2:],
+        )
+    if len(regex) >= 2 and regex[0] == "(" and ")" in regex:
+        end_index = regex.index(")")
+        or_patterns = [parse(regex) for regex in "".join(regex[1:end_index]).split("|")]
+        after_part = regex[end_index + 1 :]
+        for pattern in or_patterns:
+            if match_here(text, pattern + after_part):
                 return True
         return False
-    else:
-        return match_pattern(input_line[1:], pattern)
+    if text and match_character(text[0], regex[0]):
+        return match_here(text[1:], regex[1:])
+    return False
+def match_plus(char, text, regex):
+    if not match_character(text[0], char[0]):
+        return False
+    while text and match_character(text[0], char[0]):
+        text = text[1:]
+    return match_here(text, regex)
+def match_star(char, text, regex):
+    while text and match_character(text[0], char[0]):
+        text = text[1:]
+    return match_here(text, regex)
 def main():
     pattern = sys.argv[2]
-    input_line = sys.stdin.read()
+    text = sys.stdin.read().rstrip("\n")
     if sys.argv[1] != "-E":
-        print("Expected first argument to be '-E'")
         exit(1)
-    if match_pattern(input_line, pattern):
+    if match_pattern(text, pattern):
         exit(0)
-    else:
-        exit(1)
+    exit(1)
+
 if __name__ == "__main__":
     main()
